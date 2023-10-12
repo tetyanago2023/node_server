74544ca4b26fb38efd392e5f849542c6e7572926
go2023 <tetyanago2023@gmail.com>
2023-10-11 23:42:47 -0700

    jwt, auth, refreshtoken, logout features were implemented

--- server.js
+++ server.js
@@ -5,15 +5,25 @@ const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
+const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
+const credentials = require("./middleware/credentials");
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
 
 //Cross Origin Resource Sharing
 app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
+
+//middleware for cookies
+app.use(cookieParser());
 
 // built-in middleware for json
 app.use(express.json());
@@ -25,6 +35,10 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 app.use('/', require('./routes/root'));
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
+
+app.use(verifyJWT);
 app.use('/employees', require('./routes/api/employees'));
 
 app.all('*', (req, res) => {
